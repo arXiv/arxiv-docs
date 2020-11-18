73457975eff95833a67509f4c9a975cc805b3620
Brian D. Caruso <bdc34@cornell.edu>
2020-11-18 13:01:24 -0500

    Working on cloud build

--- mklabs.sh
+++ mklabs.sh
@@ -3,9 +3,15 @@ set -e
 # Gets required python libraries and builds labs site.
 
 if [ ! -e ".git" ]
-   then
-       echo "Must run in root of arxiv-docs repo"
-       exit 1
+then
+    echo "Must run in root of arxiv-docs repo"
+    exit 1
+fi
+
+if [ -f "/etc/debian_version" ]
+then
+    # install some build tools needed in GCP
+    sudo apt-get install python3-dev python3-pip python3-venv python3-wheel -y
 fi
 
 VIRTUAL_ENV=/tmp/arxiv-docs-venv
