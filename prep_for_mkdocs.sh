f02d03bdce7ca71784571868be2cd06594ec2a55
Brian D. Caruso <bdc34@cornell.edu>
2022-04-05 08:40:32 -0400

    Adds docs to generate_mkdocs_template.html

--- prep_for_mkdocs.sh
+++ prep_for_mkdocs.sh
@@ -7,11 +7,14 @@ if [ ! -e ".git" ]
 fi
    
 # install py packages
-pip3 install -r theme_generator/requirements.txt
+pip3 install -q -r theme_generator/requirements.txt
 
 # use arxiv-base to build mkdocs custom theme
 mkdir -p mkdocs/arxiv_doc_theme
 cd theme_generator
 FLASK_APP=app.py flask generate mkdocs_template > ../mkdocs/arxiv_doc_theme/main.html
 cd ..
+
+
+echo "If successful the template is now in mkdocs/arxiv_doc_theme/main.html"
 
