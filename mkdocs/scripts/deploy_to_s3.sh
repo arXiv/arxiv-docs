b8b938a9a834ce0115b8283a362aa20c8094ad17
Brian D. Caruso <bdc34@cornell.edu>
2022-04-08 16:21:44 -0400

    workflow

--- mkdocs/scripts/deploy_to_s3.sh
+++ mkdocs/scripts/deploy_to_s3.sh
@@ -40,33 +40,32 @@ mkdocs build
 
 pip install aws
 
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_REGION}
-text
-EOF
+# # Create a dedicated profile for this action to avoid conflicts
+# # with past/future actions.
+# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+# ${AWS_ACCESS_KEY_ID}
+# ${AWS_SECRET_ACCESS_KEY}
+# ${AWS_REGION}
+# text
+# EOF
 
 SOURCE_DIR=site
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 # might need --acl public-read in ENDPOINT_APPEND
-sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile s3-sync-action \
               --no-progress \
-              --acl public-read \
-               $*"
+              --acl public-read 
 
 # Clear out credentials after we're done.
 # We need to re-run `aws configure` with bogus input instead of
 # deleting ~/.aws in case there are other credentials living there.
 # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
+# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
+# null
+# null
+# null
+# text
+# EOF
