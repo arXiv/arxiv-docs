40272cbfd69a17eabbd659511a294e0f54fff260
Mark Nazzaro <marknazzaro2@gmail.com>
2023-03-24 15:57:09 -0400

    Revert "First attempt"
    This reverts commit f30f8a2e772a556dd39c8d635f782393441eb71c.
    
    Switching commit back from erroneous one

---
+++ deploy/README.md
@@ -0,0 +1,6 @@
+# Setup of the buckets for arxiv-docs
+
+There are two parts. 
+
+1. make and setup the buckets see the script in this directory `setup_arxiv_docs_buckets.sh`
+2. setup the build triggers for the repos. This was done manually in Cloud Build. 
---
+++ deploy/setup_arxiv_docs_buckets.sh
@@ -0,0 +1,18 @@
+# Script to setup the arxiv-docs and arxiv-docs-prs buckets
+
+PROJECT=arxiv-production
+LOCATION=us-east1
+gsutil mb -l $LOCATION -p $PRJECT gs://arxiv-docs
+gsutil mb -l $LOCATION -p $PRJECT gs://arxiv-docs-prs
+
+gsutil iam ch allUsers:objectViewer gs://arxiv-docs
+gsutil iam ch allUsers:objectViewer gs://arxiv-docs-prs
+
+
+
+# The bucket needs to be configured to use index.html pages for bare
+# paths (that is /about is redirected to /about/index.html) and the 404
+# page:
+
+gsutil web set -m index.html -e 404.html gs://arxiv-docs
+gsutil web set -m index.html -e 404.html gs://arxiv-docs-prs
